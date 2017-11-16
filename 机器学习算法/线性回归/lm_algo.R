65f200babec9175f03287f8cebe58084f2d6bcdb
tyol2005 <tyol2005@163.com>
2017-11-16 17:48:25 +0800

    add code in linear regression
    1, build formula from dataframe
    2. evaluate the lm model
    3, draw out the relation set from lm model

--- 机器学习算法/线性回归/lm_algo.R
+++ 机器学习算法/线性回归/lm_algo.R
@@ -2,4 +2,84 @@
 # this file will define some function
 #************************************
 
-find
+
+#add test data
+df <- read.csv("D:\\workspace\\R\\机器学习算法\\test_data\\generateData.csv")
+
+
+lmloop<-function(df)
+{
+  #remove record has NA value
+  df<-na.omit(df)
+  
+  #build the formula string
+  if (length(df) == 2){
+    f<-as.formula(paste(names(df)[1],names(df)[2],sep = "~"))
+  }else{
+    f<-as.formula(paste(names(df)[1],paste(names(df)[-1],sep = "",collapse = "+"),sep = "~"))
+  }
+  print(f)
+  
+  #train the lm model
+  lm.model<-lm(f,data = df)
+  
+  #print result of lm model
+  lm.model.result<-summary(lm.model)
+  print(lm.model.result)
+  
+  #check the model target
+  lm.model.rsquraed<-lm.model.result$r.squared
+  p.value<-(1-pf(lm.model.result$fstatistic[1],lm.model.result$fstatistic[2],lm.model.result$fstatistic[3]))
+  
+  #evaluate the model
+  if (p.value<0.01){
+    if((lm.model.rsquraed>0.3) & (lm.model.rsquraed<0.6)){
+      print("***********************************")
+      print("this combination is good with lm")
+      print(f)
+      print(paste("r squraed: ",lm.model.rsquraed))
+      print(paste("p-value: ",p.value))
+    }else if((lm.model.rsquraed>=0.6) & (lm.model.rsquraed<0.8)){
+      print("***********************************")
+      print("this combination is very good with lm")
+      print(f)
+      print(paste("r squraed: ",lm.model.rsquraed))
+      print(paste("p-value: ",p.value))
+    }else if(lm.model.rsquraed>=0.8){
+      print("***********************************")
+      print("this combination is so good with lm")
+      print(f)
+      print(paste("r squraed: ",lm.model.rsquraed))
+      print(paste("p-value: ",p.value))
+    }else{
+      print("***********************************")
+      print("this combination is not fitted with lm")
+      print(f)
+    }
+    
+  }else{
+    print("***********************************")
+    print("this combination is not fitted with lm")
+    print(f)
+  }
+  #end of evaluate
+  out<-list(pv<-c(p.value),rsq<-c(lm.model.rsquraed),drawOutRelationSet(names(df),lm.model.result))
+  
+  return(out)
+} # end of function lmloop
+
+#draw out the relation set from the lm model
+drawOutRelationSet<-function(nameList,lm.model.result)
+{
+  out<-list()
+  out<-append(out,nameList[1])
+  for (i in c(1:length(nameList))){
+    if (lm.model.result$coefficients[i,4]<0.05){
+    out<-append(out,nameList[i+1])
+    }
+  }
+  return(out)
+}
+
+
+
